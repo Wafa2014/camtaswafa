5030e80493f9834b6d7d06458715d5cb9b81c70e
Wafa2014 <88704690+Wafa2014@users.noreply.github.com>
2021-08-18 23:32:50 +0800

    Add files via upload

---
+++ scripts/config_xml.js
@@ -0,0 +1,58 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2021-08-18 10:33:04 PM" dc:source="Camtasia,19.0.10,enu" dc:title="CAMT2 WAFA" tscDM:firstFrame="CAMT2_WAFA_First_Frame.png" tscDM:originId="B49DF5CB-8DDA-4AE2-BC0B-4984304FC22D" tscDM:project="CAMT2 WAFA">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="44000"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="720" stDim:w="956"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Introduction" xmpDM:startTime="0" tscDM:image="CAMT2_WAFA_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 56"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Marker 1" xmpDM:startTime="29933" tscDM:image="CAMT2_WAFA_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 56"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Marker 2" xmpDM:startTime="40033" tscDM:image="CAMT2_WAFA_Thumbnails.png" tscDM:imageindex="2" tscDM:imageoffset="0" tscDM:imagerect="150, 0, 75, 56"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="3179943F-027F-4342-8114-FE599AFD9A8E" tscIQ:authoredEmail="XHAtZwOa2jA/C2t1gZp+dOD/SCCxppUtoqYnqdCDWa4h8W7TUQMActvuzpEgzzBboCfqVXVgDK5w\
+66D1X+pt4FlAjRhU2HPpp5xxl+HeiJrJ4btYJKvVMloauaCrMbV3mSzxQfHiBFkFWFVU8b31JKPc\
+JIhvbyYjtpilSUfo21nqyvU3eQj9IRtSG2xe6S10T1aoSj8Zm47obLkDo7hOeyUMtd58K++sX+Jx\
+0hD6BFd4v++BnNs5YUtAgsZVczIixCRRWoC+AUtyNMC3OmFZ0qbZz5ez+Wu08wRcb8obwUUvT/at\
+TrlUyFJ2ABCs3v6jMVcJqFusSEEiXScgDvkdjg==" tscIQ:requireUserId="0" tscIQ:locale="en-US" tscIQ:reportMethod="NONE" tscIQ:allowSkipQuiz="0" tscIQ:clientId="E7885279-24D1-4E90-AF2A-EF9A7A4E075B" tscIQ:hideReplay="0" tscIQ:quizHash="419057f4410c8140c0b013b1481871e6">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="5800" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 1"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="SHORT" tscIQ:id="0"><tscIQ:question>BAGAIMANA KABARNYA</tscIQ:question><tscIQ:correctAnswer/><tscIQ:answerArray/></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:startTime="32133" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 2"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="SHORT" tscIQ:id="1"><tscIQ:question>GIMANA FAHAM NGGAK???</tscIQ:question><tscIQ:correctAnswer/><tscIQ:answerArray/></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="Previous"/><rdf:li xmpDM:name="txtNext" xmpDM:value="Next"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="Take Quiz Now"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="Submit Answers"/><rdf:li xmpDM:name="txtReview" xmpDM:value="Replay Last Section"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="View Answers"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="Continue"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="CAMT2 WAFA.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="CAMT2_WAFA_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="CAMT2_WAFA_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
